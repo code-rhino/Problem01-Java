eac730c2907341d1b3a8b3b94343d45103ca43f8
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2020-09-22 14:20:56 -0400

    updates

--- src/test/java/cd/get/ready/algorithms/WuTangForeverBuzzTest.java
+++ src/test/java/cd/get/ready/algorithms/WuTangForeverBuzzTest.java
@@ -14,4 +14,13 @@ public class WuTangForeverBuzzTest {
 
         Assert.assertEquals(expected,actual);
     }
+
+    public void wuTangClanTest2(){
+        WuTangForeverBuzz wuTangForeverBuzz = new WuTangForeverBuzz();
+        String expected = "1\n2\nWu\n4\nTang\nWu\n7\n8\nWu\nTang\n11\nWu\n13\n14\nWuTangForever\n16\n17\nWu\n19\nTang\nWu\n22\n"+
+                "23\nWu\nTang\n26\nWu\n28\n29\nWuTangForever\n31\n32\nWu\n34\nTang\nWu\n37\n38\nWu\nTang\n";
+        String actual = wuTangForeverBuzz.wuTangClan(40);
+
+        Assert.assertEquals(expected,actual);
+    }
 }
